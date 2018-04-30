2a89d9b77eb40b84a12a5c91a4ae9904619622f2
xiaoxuan <shawn_au@outlook.com>
2018-04-30 22:36:17 +0800

    Update train_model.py

---
+++ train_model.py
@@ -0,0 +1,138 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+import lightgbm as lgb
+import gc
+
+
+# In[2]:
+
+
+df = pd.read_csv('nextClick_180w.csv')
+
+
+# In[3]:
+
+
+train = df[df.day != 9]
+valid = df[df.day == 9]
+
+
+# In[4]:
+
+
+train.shape, valid.shape
+
+
+# In[5]:
+
+
+train.columns
+
+
+# In[6]:
+
+
+lgb_params = {
+    'boosting_type': 'gbdt',
+    'objective': 'binary',
+    'metric': 'auc',
+    'learning_rate': 0.05,
+    #'is_unbalance': 'true',  #because training data is unbalance (replaced with scale_pos_weight)
+    'num_leaves': 31,  # we should let it be smaller than 2^(max_depth)
+    'max_depth': -1,  # -1 means no limit
+    'min_child_samples': 20,  # Minimum number of data need in a child(min_data_in_leaf)
+    'max_bin': 255,  # Number of bucketed bin for feature values
+    'subsample': 0.6,  # Subsample ratio of the training instance.
+    'subsample_freq': 0,  # frequence of subsample, <=0 means no enable
+    'colsample_bytree': 0.3,  # Subsample ratio of columns when constructing each tree.
+    'min_child_weight': 5,  # Minimum sum of instance weight(hessian) needed in a child(leaf)
+    'subsample_for_bin': 200000,  # Number of samples for constructing bin
+    'min_split_gain': 0,  # lambda_l1, lambda_l2 and min_gain_to_split to regularization
+    'reg_alpha': 0,  # L1 regularization term on weights
+    'reg_lambda': 0,  # L2 regularization term on weights
+    'nthread': 8,
+    'verbose': 0,
+}
+
+
+# In[10]:
+
+
+features = [x for x in train.columns if x not in ['click_time', 'is_attributed']]
+categorical = ['ip','app', 'device', 'os', 'channel', 'hour']
+respond = 'is_attributed'
+
+xgtrain = lgb.Dataset(train[features].values, 
+                      label=train[respond].values,
+                      feature_name=features,
+                      categorical_feature=categorical
+                     )
+xgvalid = lgb.Dataset(valid[features].values, 
+                      label=valid[respond].values,
+                      feature_name=features,
+                      categorical_feature=categorical
+                     )
+
+
+# In[11]:
+
+
+params = {
+    'learning_rate': 0.05,
+    #'is_unbalance': 'true', # replaced with scale_pos_weight argument
+    'num_leaves': 31,  # 2^max_depth - 1
+    'max_depth': -1,  # -1 means no limit
+    'min_child_samples': 100,  # Minimum number of data need in a child(min_data_in_leaf)
+    'max_bin': 100,  # Number of bucketed bin for feature values
+    'subsample': 0.7,  # Subsample ratio of the training instance.
+    'subsample_freq': 1,  # frequence of subsample, <=0 means no enable
+    'colsample_bytree': 0.9,  # Subsample ratio of columns when constructing each tree.
+    'min_child_weight': 0,  # Minimum sum of instance weight(hessian) needed in a child(leaf)
+    'scale_pos_weight' :200 # because training data is extremely unbalanced
+}
+lgb_params.update(params)
+
+
+# In[12]:
+
+
+evals_results = {}
+
+bst = lgb.train(lgb_params, 
+                 xgtrain, 
+                 valid_sets=[xgvalid], 
+                 valid_names=['valid'], 
+                 evals_result=evals_results, 
+                 num_boost_round=70,
+                 early_stopping_rounds=None,
+                 verbose_eval=10, 
+                 feval=None)
+
+
+# In[13]:
+
+
+print("\nModel Report")
+print("bst1.best_iteration: ", bst.best_iteration)
+print('auc'+":", evals_results['valid']['auc'][bst.best_iteration-1])
+
+
+# In[14]:
+
+
+gain = bst.feature_importance('gain')
+ft = pd.DataFrame({'feature':bst.feature_name(), 'split':bst.feature_importance('split'), 'gain':100 * gain / gain.sum()}).sort_values('gain', ascending=False)
+ft.to_csv('nextClick_importance.csv',index=False)
+
+
+# In[ ]:
+
+
+
+
